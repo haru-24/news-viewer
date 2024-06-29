4bcb842cf21248141e52f410b7b5e84fe555daa0
Haru Nishiura <h24wler@gmail.com>
2024-06-29 15:03:50 +0900

    Add dummy data

--- App.tsx
+++ App.tsx
@@ -1,34 +1,25 @@
-import { StyleSheet, View } from "react-native";
-import { StatusBar } from "expo-status-bar";
-import { ListItem } from "./components/ListItem";
+import { StyleSheet, View } from "react-native"
+import { StatusBar } from "expo-status-bar"
+import { ListItem } from "./components/ListItem"
+import articles from "./dummies/articles.json"
 
 export default function App() {
+  const items = articles.map((article,index) => {
+    return (
+      <ListItem
+        key={index.toString()}
+        imageURL={article.urlToImage}
+        title={article.title}
+        author={article.author}
+      />
+    )
+  })
   return (
     <View style={styles.container}>
-      <ListItem
-        imageURL="https://picsum.photos/200/300"
-        title="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia voluptatibus, quae, voluptatum, quibusdam, voluptate quos accusantium quod quas voluptates inventore. Quisquam, voluptatibus voluptates. Quisquam, voluptatibus voluptates. Quisquam, voluptates voluptates."
-        author="React News"
-      />
-      <ListItem
-        imageURL="https://picsum.photos/200/600"
-        title="こんにちは"
-        author="aaa"
-      />
-      <ListItem
-        imageURL="https://picsum.photos/200/400"
-        title="こんばんは"
-        author="bbb"
-      />
-      <ListItem
-        imageURL="https://picsum.photos/200/100"
-        title="おはようございます"
-        author="xxx"
-      />
-
+      {items}
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -38,4 +29,4 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-});
+})
