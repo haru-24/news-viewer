f2a2d46ac1b2cbfc96bfc99ef021d9efc34b5aef
Haru Nishiura <h24wler@gmail.com>
2024-06-30 10:00:35 +0900

    Add stack navigator

--- App.tsx
+++ App.tsx
@@ -1,52 +1,18 @@
-import { FlatList, SafeAreaView, StyleSheet } from "react-native"
-import { StatusBar } from "expo-status-bar"
-import { ListItem } from "./components/ListItem"
-import { useEffect, useState } from "react"
-import { Article, NewsResponse } from "./types/types"
-import axios from "axios"
-import { NEWS_API_KEY } from "./env"
+import { NavigationContainer } from "@react-navigation/native"
+import { HomeScreen } from "./screens/HomeScreen"
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
 
 export default function App() {
-  const [articles, setArticles] = useState<Article[]>([])
-
-  const fetchArticles = async () => {
-    try {
-      const response = await axios.get<NewsResponse>(
-        `https://newsapi.org/v2/top-headlines?country=jp&category=business&apiKey=${NEWS_API_KEY}`
-      )
-      if (response.data.articles) {
-        setArticles(response.data.articles)
-      }
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  useEffect(() => {
-    fetchArticles()
-  }, [])
-
+  const Stack = createNativeStackNavigator()
   return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={articles}
-        renderItem={({ item }) => (
-          <ListItem
-            imageURL={item.urlToImage}
-            title={item.title}
-            author={item.author}
-          />
-        )}
-        keyExtractor={(item, index) => index.toString()}
-      />
-      <StatusBar style="auto" />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-  },
-})
---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,50 @@
+import { FlatList, SafeAreaView, StyleSheet } from "react-native"
+import { ListItem } from "../components/ListItem"
+import { useEffect, useState } from "react"
+import { Article, NewsResponse } from "../types/types"
+import axios from "axios"
+import { NEWS_API_KEY } from "../env"
+
+export function HomeScreen() {
+  const [articles, setArticles] = useState<Article[]>([])
+
+  const fetchArticles = async () => {
+    try {
+      const response = await axios.get<NewsResponse>(
+        `https://newsapi.org/v2/top-headlines?country=jp&category=business&apiKey=${NEWS_API_KEY}`
+      )
+      if (response.data.articles) {
+        setArticles(response.data.articles)
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchArticles()
+  }, [])
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={articles}
+        renderItem={({ item }) => (
+          <ListItem
+            imageURL={item.urlToImage}
+            title={item.title}
+            author={item.author}
+          />
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+})
