6541548a87c1e6d06f45b4f56a91621cb437eb5b
Haru Nishiura <h24wler@gmail.com>
2024-08-17 14:51:02 +0900

    refactor: Update ArticleScreen to use custom hooks and add deleteClip functionality

--- screens/ArticleScreen.tsx
+++ screens/ArticleScreen.tsx
@@ -4,8 +4,8 @@ import { RouteProp } from "@react-navigation/native";
 import { Article } from "../types/types";
 import WebView from "react-native-webview";
 import { ClipButton } from "../components/ClipButton";
-import { useDispatch } from "react-redux";
-import { addClip } from "../store/userSlice";
+import { addClip, deleteClip } from "../store/userSlice";
+import { useAppDispatch, useAppSelector } from "../store/hooks";
 export type ArticleStackParamList = {
   Home: undefined;
   Article: { article: Article };
@@ -16,15 +16,21 @@ type ProfileScreenRouteProp = RouteProp<ArticleStackParamList, "Article">;
 export function ArticleScreen({ route }: { route?: ProfileScreenRouteProp }) {
   if (!route) throw new Error("Route prop is required");
   const { article } = route.params;
-  const dispatch = useDispatch();
+  const dispatch = useAppDispatch();
+  const clips = useAppSelector((state) => state.user.clips);
+  const isClipped = clips.some((clip) => clip.url === article.url);
 
   const onPressClip = () => {
-    dispatch(addClip(article));
+    if (isClipped) {
+      dispatch(deleteClip(article));
+    } else {
+      dispatch(addClip(article));
+    }
   };
 
   return (
     <SafeAreaView style={styles.container}>
-      <ClipButton onPress={onPressClip} enabled={false} />
+      <ClipButton onPress={onPressClip} enabled={isClipped} />
       <WebView source={{ uri: article.url }} />
     </SafeAreaView>
   );
