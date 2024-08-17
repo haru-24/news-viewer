b9d46a1722621af19214eddac77f7969d97629be
Haru Nishiura <h24wler@gmail.com>
2024-08-17 14:41:03 +0900

    chore: Add ClipButton component and update ArticleScreen

--- screens/ArticleScreen.tsx
+++ screens/ArticleScreen.tsx
@@ -3,6 +3,9 @@ import { SafeAreaView, StyleSheet } from "react-native";
 import { RouteProp } from "@react-navigation/native";
 import { Article } from "../types/types";
 import WebView from "react-native-webview";
+import { ClipButton } from "../components/ClipButton";
+import { useDispatch } from "react-redux";
+import { addClip } from "../store/userSlice";
 export type ArticleStackParamList = {
   Home: undefined;
   Article: { article: Article };
@@ -13,9 +16,15 @@ type ProfileScreenRouteProp = RouteProp<ArticleStackParamList, "Article">;
 export function ArticleScreen({ route }: { route?: ProfileScreenRouteProp }) {
   if (!route) throw new Error("Route prop is required");
   const { article } = route.params;
+  const dispatch = useDispatch();
+
+  const onPressClip = () => {
+    dispatch(addClip(article));
+  };
 
   return (
     <SafeAreaView style={styles.container}>
+      <ClipButton onPress={onPressClip} enabled={false} />
       <WebView source={{ uri: article.url }} />
     </SafeAreaView>
   );
