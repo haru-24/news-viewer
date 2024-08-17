2f8825f6a779bfc8dbd98620c55e70e24a55f428
Haru Nishiura <h24wler@gmail.com>
2024-08-17 14:03:28 +0900

    chore: Refactor navigation structure and add bottom tab navigation

--- App.tsx
+++ App.tsx
@@ -1,12 +1,23 @@
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, ParamListBase, RouteProp } from "@react-navigation/native";
 import { HomeScreen } from "./screens/HomeScreen";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { ArticleScreen, ArticleStackParamList } from "./screens/ArticleScreen";
+import { ArticleScreen } from "./screens/ArticleScreen";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { ClipScreen } from "./screens/ClipScreen";
+import { FontAwesome } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
+
+const renderTabBarIcon = (route: RouteProp<ParamListBase, string>, color: string, size: number) => {
+  let iconName;
+
+  if (route.name === "HomeTab") {
+    return <FontAwesome name="home" size={size} color={color} />;
+  } else if (route.name === "ClipTab") {
+    return <FontAwesome name="bookmark" size={size} color={color} />;
+  }
+};
 
 const HomeStack = () => {
   return (
@@ -20,9 +31,13 @@ const HomeStack = () => {
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="HomeTab" component={HomeStack} options={{ headerShown: false }} />
-        <Tab.Screen name="ClipTab" component={ClipScreen} />
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => renderTabBarIcon(route, color, size),
+        })}
+      >
+        <Tab.Screen name="HomeTab" component={HomeStack} options={{ headerShown: false, title: "Home" }} />
+        <Tab.Screen name="ClipTab" component={ClipScreen} options={{ title: "Clip" }} />
       </Tab.Navigator>
     </NavigationContainer>
   );
